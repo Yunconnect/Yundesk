f1d1c39c3f00fef00ab18b6dccdbf8742af309c3
Yunsoft <96048201+Yunconnect@users.noreply.github.com>
2024-11-04 21:03:20 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -104,8 +104,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.ibyt.fun"];
-pub const PUBLIC_RS_PUB_KEY: &str = "6Ab15cJR7RB4KvqjCTpRpRQLVCzAncexxJ0sDxRoi2k=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["oalc2735.cnvpn.org"];
+pub const PUBLIC_RS_PUB_KEY: &str = "cg5+yfFgpNxOEUKFZAH2t1BgCi6ZXAV1WdzaCFisC3Q=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
